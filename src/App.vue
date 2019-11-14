24079f6f9ce3d12283beb4e6409986b56ca1a57d
Patryk Pekala <ppekala@icloud.com>
2019-11-14 16:54:05 +0100

    Add basic routes, styling and home page

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,42 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+	<div id="app">
+		<div id="nav">
+			<router-link to="/">Home</router-link>
+			<router-link to="/features">Features</router-link>
+			<router-link to="/privacy">Privacy</router-link>
+			<router-link to="/support">Support</router-link>
+			<router-link to="/download">Download</router-link>
+		</div>
+		<router-view />
+	</div>
 </template>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+	font-family: 'HelveticaNeue-CondensedBold';
+	font-size: 20px;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: black;
 }
 
 #nav {
-  padding: 30px;
+	display: flex;
+	padding: 30px;
+	justify-content: space-evenly;
 }
 
 #nav a {
-  font-weight: bold;
-  color: #2c3e50;
+	color: black;
+	text-decoration: none;
 }
 
 #nav a.router-link-exact-active {
-  color: #42b983;
+	color: red;
+}
+
+.description {
+	margin: 1em 5em;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div class="home">
+		<img
+			alt="Apple Watch and iPhone with the DuoCycle apps"
+			src="../assets/Hero-Home.png"
+		/>
+		<p class="description">
+			DuoCycle is a novel fitness app that tracks your rides with Apple Watch
+			and shows all of your workout data live on iPhone. Your Apple Watch and
+			iPhone working as a perfect Duo, designed with cyclists in mind.
+		</p>
+		<img
+			alt="Apple Watch and iPhone with the DuoCycle apps"
+			src="../assets/AppStoreBadge.png"
+		/>
+	</div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+	name: 'home'
+};
 </script>
